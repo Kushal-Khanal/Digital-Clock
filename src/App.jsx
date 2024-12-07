55633c29c1c3538772e906785b16fa628962fd7b
Kushal Khanal <khanalkushal72@gmail.com>
2024-12-07 21:26:15 +0545

    origin

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import DigitalClock from "./DigitalClock.jsx"
+import Stopwatch from "./Stopwatch.jsx"
+
+function App() {
+  
+
+  return (
+    <>
+     <DigitalClock/>
+     <Stopwatch/>
+     <Stopwatch/>
+    </>
+  )
+}
+
+export default App
